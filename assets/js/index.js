57db10b0df05b0121569e16b93e8aace90db06fd
Zain0-0 <122361692+Zain0-0@users.noreply.github.com>
2025-01-29 21:27:50 +0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+
+
+// function myFunction(){
+      
+//     let genKey = document.getElementById("gen-key-button-id");    
+//        genKey.style.display = "none";
+//        genKey.style.backdropFilter = "none";
+//     document.getElementById("token-selection-id").style.display = "block";
+//     document.getElementById("holding-section").style.display = "block";
+//     document.getElementById("active-section").style.display = "block";
+//     document.getElementById("fire-ball-pepe").style.display = "none";
+//     document.getElementById("fire-ball-king").style.display = "flex";
+       
+// }
+
